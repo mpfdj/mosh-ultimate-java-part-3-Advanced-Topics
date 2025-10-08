43d0ee78e0f027c0ecaf953e7ba1ef0bd7c4c6bc
Miel de Jaeger <miel.de.jaeger@ing.com>
2025-10-08 12:07:08 +0200

    Added some more examples on collections

---
+++ codewithmosh/src/com/codewithmosh/collections/Main.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SetDemo.show();
+    }
+
+
+
+}
--- codewithmosh/src/com/codewithmosh/collections/SetDemo.java
+++ codewithmosh/src/com/codewithmosh/collections/SetDemo.java
@@ -4,19 +4,25 @@ import java.util.*;
 
 public class SetDemo {
   public static void show() {
-    Set<String> set1 =
-      new HashSet<>(Arrays.asList("a", "b", "c"));
-
-    Set<String> set2 =
-      new HashSet<>(Arrays.asList("b", "c", "d"));
+    Set<String> set1;
+    Set<String> set2;
 
     // Union
+    set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
+    set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
     set1.addAll(set2);
+    System.out.println(set1);
 
     // Intersection
+    set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
+    set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
     set1.retainAll(set2);
+    System.out.println(set1);
 
     // Difference
+    set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
+    set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
     set1.removeAll(set2);
+    System.out.println(set1);
   }
 }
